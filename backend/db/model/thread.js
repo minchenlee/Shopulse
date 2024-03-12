7f8903001f2eecfa27ff29aead9cc3d80e239d48
minchenlee <leeminchen.vincent@gmail.com>
2024-03-12 18:09:48 +0800

    Revert "Modify: update CORS originFeat/backend"

--- backend/db/model/thread.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const threadSchema = require('../schema/thread');
-
-// Create a model
-const ThreadModel = mongoose.model('Thread', threadSchema);
-
-module.exports = ThreadModel
